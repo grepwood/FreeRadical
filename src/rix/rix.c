d8f029458f24b30e6b10aa71e8f490d05ed87989
grepwood <grepwood@sucs.org>
2014-03-09 17:59:50 +0000

    The included main demonstrates properly SDL_Load'ing a RIX3 from RAM

--- src/rix/rix.c
+++ src/rix/rix.c
@@ -1,9 +1,12 @@
+#ifndef _GNU_SOURCE
+#	define _GNU_SOURCE
+#endif
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
-#include <png.h>
 
+#include <SDL2/SDL.h>
 #include <rix/rix.h>
 #include <dataio/BigEndian.h>
 
@@ -39,62 +42,25 @@ char IsFalloutRIX(FILE * Splash) /* File must be set on beginning */
 	fseeko(Splash,0,SEEK_SET);
 	if(!strncmp(RIX.Signature,"RIX3",4))
 	{
-		result += 0b00000001;
+		result += 1;
 	}
 	if(ActualSize == 307978)
 	{
-		result += 0b00000010;
+		result += 2;
 	}
 	if(RIX.Width == 640)
 	{
-		result += 0b00000100;
+		result += 4;
 	}
 	if(RIX.Height == 480)
 	{
-		result += 0b00001000;
+		result += 8;
 	}
 	if(RIX.Unknown == 0x00AF)
 	{
-		result += 0b00010000;
+		result += 16;
 	}
 	return result;
-}
-
-
-
-/* This function works on the assumption that IsFalloutRIX returned 31 */
-
-void FalloutRIX2BMPFile(FILE * RIX, FILE * bmp)
-{
-	char RIXPalette[768];
-	char bitmap[921600];	/* This is where we store all 640x480x24 pixels */
-	char OrderedPixel[3];	/* We need this because BMP stores in BGR while RIX stores in RGB */
-	int32_t Position;
-	uint16_t Counter;
-	uint16_t Index;
-/* Simple BMP header for a 640x480x24 pic */
-	char bmpheader[54] = {	0x42,0x4D,0x36,0x10,0x0E,0x00,0x00,0x00,
-							0x00,0x00,0x36,0x00,0x00,0x00,0x28,0x00,
-							0x00,0x00,0x80,0x02,0x00,0x00,0xE0,0x01,
-							0x00,0x00,0x01,0x00,0x18,0x00,0x00,0x00,
-							0x00,0x00,0x00,0x10,0x0E,0x00,0x13,0x0B,
-							0x00,0x00,0x13,0x0B,0x00,0x00,0x00,0x00,
-							0x00,0x00,0x00,0x00,0x00,0x00};
-	fwrite(bmpheader,54,1,bmp);
-	fseeko(RIX,10,SEEK_SET);
-	fread(RIXPalette,768,1,RIX);
-	for(Position = 919680; Position >= 0; Position -= 1920)
-	{
-		for(Counter = 0; Counter < 1920; Counter += 3)
-		{
-			Index = 3*fgetc(RIX);
-			OrderedPixel[0] = *(RIXPalette+Index+2);
-			OrderedPixel[1] = *(RIXPalette+Index+1);
-			OrderedPixel[2] = *(RIXPalette+Index);
-			memcpy(bitmap+Position+Counter,OrderedPixel,3);
-		}
-	}
-	fwrite(bitmap,921654,1,bmp);
 }
 
 char * FalloutRIX2BMPMem(FILE * RIX)
@@ -106,13 +72,8 @@ char * FalloutRIX2BMPMem(FILE * RIX)
 	uint16_t Counter;
 	uint16_t Index;
 /* Simple BMP header for a 640x480x24 pic */
-	char bmpheader[54] = {	0x42,0x4D,0x36,0x10,0x0E,0x00,0x00,0x00,
-							0x00,0x00,0x36,0x00,0x00,0x00,0x28,0x00,
-							0x00,0x00,0x80,0x02,0x00,0x00,0xE0,0x01,
-							0x00,0x00,0x01,0x00,0x18,0x00,0x00,0x00,
-							0x00,0x00,0x00,0x10,0x0E,0x00,0x13,0x0B,
-							0x00,0x00,0x13,0x0B,0x00,0x00,0x00,0x00,
-							0x00,0x00,0x00,0x00,0x00,0x00};
+	char bmpheader[54] = {	66,77,54,16,14,0,0,0,0,0,54,0,0,0,40,0,0,0,128,2,0,0,224,1,0,0,
+				1,0,24,0,0,0,0,0,0,16,14,0,19,11,0,0,19,11,0,0,0,0,0,0,0,0,0,0};
 	memcpy(bitmap,bmpheader,54);
 	fseeko(RIX,10,SEEK_SET);
 	fread(RIXPalette,768,1,RIX);
@@ -121,28 +82,80 @@ char * FalloutRIX2BMPMem(FILE * RIX)
 		for(Counter = 0; Counter < 1920; Counter += 3)
 		{
 			Index = 3*fgetc(RIX);
-			OrderedPixel[0] = *(RIXPalette+Index+2);
-			OrderedPixel[1] = *(RIXPalette+Index+1);
-			OrderedPixel[2] = *(RIXPalette+Index);
+			OrderedPixel[0] = 4*(*(RIXPalette+Index+2));
+			OrderedPixel[1] = 4*(*(RIXPalette+Index+1));
+			OrderedPixel[2] = 4*(*(RIXPalette+Index));
 			memcpy(bitmap+Position+Counter,OrderedPixel,3);
 		}
 	}
 	return bitmap;
 }
 
+void logSDLError(char * os, const char * msg){
+	printf("%s%s error: %s\n",os,msg,SDL_GetError());
+}
+
 int main(int argc, char * argv[])
 {
 	FILE * fp = NULL;
-	FILE * bmp = NULL;
+	int result = 0;
 	char * bitmap = NULL;
-	if(argc != 3){ exit(1); }
+	/* SDL stuff */
+	SDL_Window * win = NULL;
+	SDL_Renderer * ren = NULL;
+	SDL_Surface * bmp = NULL;
+	SDL_Texture * tex = NULL;
+	SDL_RWops * rixmap = NULL;
+	/* Declarations end */
+	if(argc != 2){ exit(1); }
 	fp = fopen(argv[1],"rb");
-	bmp = fopen(argv[2],"wb");
-//	FalloutRIX2bmp(fp,bmp);
-	bitmap = FalloutRIX2BMPMem(fp);
-	fwrite(bitmap,921654,1,bmp);
-	free(bitmap);
+	if(IsFalloutRIX(fp) == 31)
+	{
+		/* Preparing */
+		if(SDL_Init(SDL_INIT_EVERYTHING))
+		{
+			printf("SDL_Init Error: %s\n",SDL_GetError());
+			result += 1;
+		}
+		win = SDL_CreateWindow("Hello RIX3!",100,100,640,480,SDL_WINDOW_SHOWN);
+		if(win == NULL)
+		{
+			printf("SDL_CreateWindow Error: %s\n",SDL_GetError());
+			result += 2;
+		}
+		ren = SDL_CreateRenderer(win,-1,SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+		if(ren == NULL)
+		{
+			printf("SDL_CreateRenderer Error: %s\n",SDL_GetError());
+			result += 4;
+		}
+		bitmap = FalloutRIX2BMPMem(fp);
+		rixmap = SDL_RWFromConstMem(bitmap,921654);
+		bmp = SDL_LoadBMP_RW(rixmap,1);
+		if(bmp == NULL)
+		{
+			printf("SDL_LoadBMP Error: %s\n",SDL_GetError());
+			result += 8;
+		}
+		tex = SDL_CreateTextureFromSurface(ren, bmp);
+		SDL_FreeSurface(bmp);
+		if (tex == NULL)
+		{
+			printf("SDL_CreateTextureFromSurface Error: %s\n",SDL_GetError());
+			result += 16;
+		}
+		/* Drawing */
+		SDL_RenderClear(ren);
+		SDL_RenderCopy(ren, tex, NULL, NULL);
+		SDL_RenderPresent(ren);
+		SDL_Delay(2000);
+		/* Cleaning up */
+		SDL_DestroyTexture(tex);
+		SDL_DestroyRenderer(ren);
+		SDL_DestroyWindow(win);
+		SDL_Quit();
+		free(bitmap);
+	}
 	fclose(fp);
-	fclose(bmp);
-	return 0;
+	return result;
 }
