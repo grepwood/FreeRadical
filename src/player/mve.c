2afb338319b397069af13e7cab15dd801f5e78ef
Michael Dec <grepwood@sucs.org>
2014-03-10 15:12:53 +0000

    no overflows and strict compare

--- src/player/mve.c
+++ src/player/mve.c
@@ -2,11 +2,11 @@
 #include <string.h>
 
 /* Will return 0 (false) if file is MVE */
-char FileIsNotMVE(FILE * Movie)
+int FileIsNotMVE(FILE * Movie)
 {
 	char Control[20];
 	fseeko(Movie,0,SEEK_SET);
 	fread(Control,20,1,Movie);
 	fseeko(Movie,0,SEEK_SET);
-	return (char)strncmp(Control,"Interplay MVE File\x1A\0",20);
+	return memcmp(Control,"Interplay MVE File\x1A\0",20);
 }
