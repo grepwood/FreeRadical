ee63a7984f7190065f6f14c6ac9eb88ff865db5c
grepwood <grepwood@sucs.org>
2014-09-10 00:19:06 +0100

    fixed bug where FalloutRIX2BMPMem would segfault

--- include/rix/rix.h
+++ include/rix/rix.h
@@ -2,24 +2,13 @@
 
 /* Note: all values in a RIX3 file are little endian */
 
-struct RIXheader
-{
+unsigned char * FalloutRIX2BMPMem(FILE * RIX);
+char IsFalloutRIX(FILE * Splash);
+
+struct RIXheader {
 	char Signature[4];	/* file signature (RIX3 in ASCII) */
 	uint16_t Width; 	/* width of the image (640 pixels, 0x0280) */
 	uint16_t Height; 	/* height of the image (480 pixels, 0x01E0) */
-	uint16_t Unknown;	/* Unknown. Possibly the number of colors used.
-						 * Fallout 2 uses only one value (0x00AF) in the RIX file header.
-						 * Suppose we can use it as a control value? */
-};
-
-struct RGB
-{
-	unsigned char Red;		/* Red intensity */
-	unsigned char Green;	/* Green intensity */
-	unsigned char Blue;		/* Blue intensity */
-};
-
-struct Palette
-{
-	char entries[768]; /* 3*256 */
+	uint8_t PT;			/* Palette type. 0xAF specifies VGA and is the expected value. */
+	uint8_t ST;			/* Storage type. 0x00 specifies linear storage. */
 };
