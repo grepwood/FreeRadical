84c20310a42eae8e169dbeb1e4ee8dc6a1a8ad03
Michael Dec <grepwood@sucs.swan.ac.uk>
2014-11-12 15:13:43 +0000

    shrinking SLOC

--- src/dataio/DataSize.c
+++ src/dataio/DataSize.c
@@ -2,25 +2,19 @@
 
 #include "include/dataio/BigEndian.h"
 
-uint32_t DAT2DataSize(FILE * DAT2)
-{
+uint32_t DAT2DataSize(FILE * DAT2) {
 	uint32_t result;
 	fseeko(DAT2,-4,SEEK_END);
 	fread(&result,4,1,DAT2);
-	if(!BigEndian()) {
-		result = FR_bswap32(result);
-	}
+	if(!BigEndian()) result = FR_bswap32(result);
 	return result;
 }
 
-uint32_t DAT2TreeSize(FILE * DAT2)
-{
+uint32_t DAT2TreeSize(FILE * DAT2) {
 	uint32_t result;
 	fseeko(DAT2,-8,SEEK_END);
 	fread(&result,4,1,DAT2);
-	if(!BigEndian()) {
-		result = FR_bswap32(result);
-	}
+	if(!BigEndian()) result = FR_bswap32(result);
 	return result;
 }
 
@@ -28,19 +22,15 @@ uint32_t DAT2TreeSize(FILE * DAT2)
  * like
  * DAT2FilesTotal(DAT2File,DataSize-TreeSize-8); */
 
-uint32_t DAT2FilesTotal(FILE * DAT2, uint32_t Offset)
-{
+uint32_t DAT2FilesTotal(FILE * DAT2, uint32_t Offset) {
 	uint32_t result;
 	fseeko(DAT2,Offset,SEEK_SET);
 	fread(&result,4,1,DAT2);
-	if(!BigEndian()) {
-		result = FR_bswap32(result);
-	}
+	if(!BigEndian()) result = FR_bswap32(result);
 	return result;
 }
 
-char ItIsDAT2(FILE * DATX)
-{
+char ItIsDAT2(FILE * DATX) {
 	char result = 0;
 	uint32_t ActualSize = 0;
 	uint32_t Current = ftello(DATX);
@@ -48,9 +38,6 @@ char ItIsDAT2(FILE * DATX)
 	fseeko(DATX,0L,SEEK_END);
 	ActualSize = ftello(DATX);
 	fseeko(DATX,Current,SEEK_SET); /* We gracefully restore position */
-	if(ActualSize == DataSize)
-	{
-		result = 1; /* Or an unlikely coincidence */
-	}
+	if(ActualSize == DataSize) result = 1; /* Or an unlikely coincidence */
 	return result;
 }
